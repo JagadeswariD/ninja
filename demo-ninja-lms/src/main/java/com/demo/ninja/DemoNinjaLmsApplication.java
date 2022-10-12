220e08cdd4242363ee1bce3ace740ac90f84e642
Djeswari <djesw@DESKTOP-5A3Q1QR.attlocal.net>
2022-10-12 11:07:28 -0400

    Base Project Creation

---
+++ demo-ninja-lms/src/main/java/com/demo/ninja/DemoNinjaLmsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNinjaLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNinjaLmsApplication.class, args);
+	}
+
+}
